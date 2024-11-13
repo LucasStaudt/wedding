e1b0cb71ed68b49acb0bc3d7780fe454741c9ae7
unknown <lucas.std1@gmail.com>
2024-11-12 22:05:50 -0300

    add outdir in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/wedding/',
   build: {
     outDir: 'build',
     chunkSizeWarningLimit: 1000,
